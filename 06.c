27129303a5602da7001659e2c134744305186618
taina <tainaml@live.ca>
2016-01-24 17:46:14 -0300

    Área Retângulo

---
+++ 06.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main () {
+
+int b,a,area;
+
+printf ("Base do retangulo: ");
+scanf("%d", &b);
+printf("Altura do retangulo: ");
+scanf("%d", &a);
+
+area = b*a;
+
+printf("Area do retangulo: %d ",area);
+
+return 0;
+
+}
