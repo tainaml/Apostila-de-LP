8af6f793211973b71f108d19707a900fce2d1b62
taina <tainaml@live.ca>
2016-02-04 18:13:36 -0300

    15

---
+++ 15.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main () {
+
+float number;
+
+printf("Digite um numero: ");
+scanf("%f", &number);
+
+if (number < 0) {
+    printf("NUMERO NEGATIVO\n");
+}
+else 
+{
+    printf("NUMERO POSITIVO\n");
+}
+
+}
+
+
+
+
+
+
+
+
+
