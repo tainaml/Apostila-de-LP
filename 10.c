73270216c8867c7d01f54cafa624435c279714cd
taina <tainaml@live.ca>
2016-02-04 17:21:23 -0300

    10

---
+++ 10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main () {
+
+    float = c_carro,c_fabrica,porc_dist, porc_imp;
+   
+    printf("Custo de Fabrica :");
+    scanf("%f",&c_fabrica);
+
+    c_carro = (c_fabrica * 0.28) * 0.45;
+
+    printf("O custo final: ",c_carro);
+
+}
+   
+
